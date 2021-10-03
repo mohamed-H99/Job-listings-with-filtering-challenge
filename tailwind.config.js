9382fa4674e948daa4dd1820b53286712d8c1eb7
ahmdbhgat4 <ahmdbhgat4@gmail.com>
2021-10-03 18:12:25 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      "cyan-dark": "#5ba4a4",
+      "grayish-light": "#effafa",
+      "grayish-lighter": "#eef6f6",
+      "grayish-dark": "#7b8e8e",
+      "grayish-darker": "#2c3a3a",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
